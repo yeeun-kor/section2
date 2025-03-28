c787aa2e2735865e85dde80012578cb35e1b6e5c
yeeun <jovm268@naver.com>
2025-03-28 18:13:38 +0900

    ss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 export default function Home() {
   return (
     <>
-      <h1 className={style.h1}>인덱스</h1>
+      <h1 className={style.h1}>인덱</h1>
       <h2 className={style.h2}>메인페이지</h2>
     </>
   );
