e1d21d41dc9d240d92b1aa31249fbf3bda5a9f08
yeeun-kor <jovm268@naver.com>
2025-04-02 15:34:45 +0900

    api로 전체도서목록조회회

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,22 @@ import style from "./index.module.css";
 import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const allBooks = await fetchBooks();
+
+  return {
+    props: { allBooks },
+  };
+};
+
+export default function Home({
+  allBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allBooks);
+
   return (
     <div className={style.container}>
       <section>
@@ -15,7 +29,7 @@ export default function Home() {
       </section>
       <section>
         <h3>등록된 모든 도서 </h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
